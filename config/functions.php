e836bb33fa2faef9d6ea1b0ebd556e60f339a2dc
ptrckc10 <ptrckc10@gmail.com>
2020-11-03 18:56:54 +0800

    latest

--- config/functions.php
+++ config/functions.php
@@ -273,7 +273,7 @@ else if (($_SERVER["REQUEST_METHOD"] == "POST") && (isset($_POST["register_butto
 
         // Validate age
         $birthdate = new DateTime($bdate);
-        $today   = new DateTime('today');
+        $today = new DateTime('today');
         $age = $birthdate->diff($today)->y;
         if ($age < 12) {
             $bdate_err = "User must be 12y/o and above.";
