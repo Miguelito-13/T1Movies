341a12a40f9ce304734d77d9e1c0a5df9703a580
Julius Villa <68671594+papajuju8@users.noreply.github.com>
2020-11-05 00:32:03 +0800

    Update header.php

--- views/header.php
+++ views/header.php
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="../css/style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../css/movies_style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../css/genre_style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../css/announcements_style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../css/footer_style.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
