5b52e449af58224e9b3fe543bb5697c48f436b91
Julius Villa <68671594+papajuju8@users.noreply.github.com>
2020-11-05 04:21:10 +0800

    Add files via upload

---
+++ views/announcements.php
@@ -0,0 +1,47 @@
+<section>
+    <div id="carouselAnnouncements" class="carousel slide" data-ride="carousel"">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselAnnouncements" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselAnnouncements" data-slide-to="1"></li>
+            <li data-target="#carouselAnnouncements" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="container-fluid d-block w-100 carousel-announcement" alt="First slide">
+                    <div class="row align-items-center">
+                        <div class="col-4 d-none d-md-block my-auto">
+                            <img src="../images/carousel-image1.svg" class="carousel-image-desktop"/> <!-- landscape movie poster/announcement -->
+                        </div>
+                        <div class="col d-md-none my-auto justify-content-center">
+                            <img src="../images/carousel-image1.svg" class="carousel-image-mobile"/> <!-- portrait movie poster/announcement -->
+                        </div>
+                        <div class="col-8 d-none d-md-block">
+                            <h1>BOOKING A TICKET</h1>
+                            <h2>IS AS EASY AS 1, 2 & 3!</h2>
+                            <h3>1. Select Movie</h3>
+                            <h3>2. Select Theater</h3>
+                            <h3>3. Select Date & Time</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <img class="d-block w-100" src="../images/sample_banner.svg" alt="Second slide">
+            </div>
+
+            <div class="carousel-item">
+                <img class="d-block w-100" src="../images/sample_banner.svg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselAnnouncements" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselAnnouncements" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
