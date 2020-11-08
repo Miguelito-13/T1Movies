0d8868f22c4bebc46be15f6a54fcc6b1b859533d
Julius Villa <68671594+papajuju8@users.noreply.github.com>
2020-11-08 21:18:42 +0800

    Update announcements.php

--- views/announcements.php
+++ views/announcements.php
@@ -4,11 +4,13 @@
             <li data-target="#carouselAnnouncements" data-slide-to="0" class="active"></li>
             <li data-target="#carouselAnnouncements" data-slide-to="1"></li>
             <li data-target="#carouselAnnouncements" data-slide-to="2"></li>
+			<li data-target="#carouselAnnouncements" data-slide-to="3"></li>
+			<li data-target="#carouselAnnouncements" data-slide-to="4"></li>
         </ol>
 
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <div class="container-fluid d-block w-100 carousel-announcement" alt="First slide">
+                <div class="container-fluid d-block w-100 carousel-announcement" alt="Booking a Ticket">
                     <div class="row align-items-center">
                         <div class="col-4 d-none d-md-block my-auto">
                             <img src="../images/carousel-image1.svg" class="carousel-image-desktop"/> <!-- landscape movie poster/announcement -->
@@ -19,21 +21,66 @@
                         <div class="col-8 d-none d-md-block">
                             <h1>BOOKING A TICKET</h1>
                             <h2>IS AS EASY AS 1, 2 & 3!</h2>
-                            <h3>1. Select Movie</h3>
-                            <h3>2. Select Theater</h3>
-                            <h3>3. Select Date & Time</h3>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="carousel-item">
-                <img class="d-block w-100" src="../images/sample_banner.svg" alt="Second slide">
+                <div class="container-fluid d-block w-100 carousel-announcement" alt="STEP 1: Select a Movie">
+                    <div class="row align-items-center">
+                        <div class="col-4 d-none d-md-block my-auto">
+                            <img src="../images/carousel-image1-1.svg" class="carousel-image-desktop"/> <!-- landscape movie poster/announcement -->
+                        </div>
+                        <div class="col d-md-none my-auto justify-content-center">
+                            <img src="../images/carousel-image1-1.svg" class="carousel-image-mobile"/> <!-- portrait movie poster/announcement -->
+                        </div>
+                        <div class="col-8 d-none d-md-block">
+                            <h2>STEP 1: Select a Movie</h2>
+                            <h3></h3>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div class="carousel-item">
-                <img class="d-block w-100" src="../images/sample_banner.svg" alt="Third slide">
+                <div class="container-fluid d-block w-100 carousel-announcement" alt="STEP 2: Select a Theater">
+                    <div class="row align-items-center">
+                        <div class="col-4 d-none d-md-block my-auto">
+                            <img src="../images/carousel-image1-2.svg" class="carousel-image-desktop"/> <!-- landscape movie poster/announcement -->
+                        </div>
+                        <div class="col d-md-none my-auto justify-content-center">
+                            <img src="../images/carousel-image1-2.svg" class="carousel-image-mobile"/> <!-- portrait movie poster/announcement -->
+                        </div>
+                        <div class="col-8 d-none d-md-block">
+                            <h2>STEP 2: Select a Theater</h2>
+                            <h3></h3>
+                        </div>
+                    </div>
+                </div>
             </div>
+			
+			<div class="carousel-item">
+                <div class="container-fluid d-block w-100 carousel-announcement" alt="Select Date & Time">
+                    <div class="row align-items-center">
+                        <div class="col-4 d-none d-md-block my-auto">
+                            <img src="../images/carousel-image1-3.svg" class="carousel-image-desktop"/> <!-- landscape movie poster/announcement -->
+                        </div>
+                        <div class="col d-md-none my-auto justify-content-center">
+                            <img src="../images/carousel-image1-3.svg" class="carousel-image-mobile"/> <!-- portrait movie poster/announcement -->
+                        </div>
+                        <div class="col-8 d-none d-md-block">
+                            <h2>STEP 3: Select Date & Time</h2>
+                            <h3></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+			
+			<div class="carousel-item">
+                <img class="d-block w-100" src="../images/sample_banner.svg" alt="Second slide">
+            </div>
+			
         </div>
         <a class="carousel-control-prev" href="#carouselAnnouncements" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
